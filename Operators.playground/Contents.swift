1e2c25fa6e1592a35e26bb9fa126c680d69f6b7a
svizor <andreydovgy@gmail.com>
2024-11-24 21:14:50 +0200

    If-Else

--- Operators.playground/Contents.swift
+++ Operators.playground/Contents.swift
@@ -10,3 +10,24 @@ if myAge > yourAge {
 } else {
     "Oh hey, we are the same age"
 }
+
+let myMotherAge = myAge + 22
+let doubleMyAge = myAge * 2
+
+/// 1. unary prefix
+let foo = !true
+
+/// 2. unary postfix
+let name = Optional("svizor")
+type(of: name)
+let unaryPostFix = name!
+type(of: unaryPostFix)
+
+/// 3. binary infix
+let result = 1 + 2
+let names = "Foo" + " " + "Bar"
+
+/// ternary operator
+let message = myAge > 18
+    ? "You are an adult"
+    : "You are not yet an adult"
